4fcb493802075d6cc786322b031644651727b1d6
Fabio Santos <fabioprogramadorti@gmail.com>
2018-09-10 22:11:10 -0300

    ...

--- model/user.php
+++ model/user.php
@@ -13,6 +13,39 @@ class User(){
 		$this->email = $email;
 	}
 
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getFirstName(){
+		return $this->firstName;
+	}
+
+	public function setFirstName($firstName){
+		$this->firstName = $firstName;
+	}
+
+	public function getLastName(){
+		return $this->lastName;
+	}
+
+	public function setLastName($lastName){
+		$this->lastName = $lastName;
+	}
+
+	public function getEmail(){
+		return $this->$email;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+
 	public function listUsers(){
 		query = "SELECT id, name FROM users";
 		$connection = Connection::getConnection();
