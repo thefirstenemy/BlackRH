65dbfa4aa250fb7b46e4db35472ae535ac2d4a34
Fabio Santos <fabioprogramadorti@gmail.com>
2018-09-14 22:10:53 -0300

    Modificações do sistema de rh, colocado index, informações na view, como tela inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// require_once 'model/user_model.inc.php';
+// $user_controller = new UserController ();
+// @$op = $_REQUEST ['method'] ? $_REQUEST ['method'] : $_REQUEST ['op'];
+// switch ($op) {	
+// 	case 'recovery' :
+// 		header ( "Location:view/recovery_password.php" );
+// 		break;
+// 	default :
+// 		header ( "Location:view/home.php" );
+// 		break;
+// }
+header ( "Location:view/home.php" );
+?>
