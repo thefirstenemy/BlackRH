0bc01a303e8d5e9e788f29b832134d0cdef6ddd2
Fabio Santos <fabioprogramadorti@gmail.com>
2018-09-17 22:52:51 -0300

    Update de arquivos

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="style/theme/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="icon" href="style/logo/favicon.ico" type="image/x-icon"/>
     <!-- Custon BlackRH -->
     <link href="style/blackrh_style.css" rel="stylesheet">
     <!-- Custon Bootstrap Social -->
@@ -36,7 +36,9 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Black RH</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+          <img class="img-fluid brh-header-logo" src="style/logo/logo_black_rh.png" alt="">
+        </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -44,12 +46,15 @@
           <ul class="navbar-nav ml-auto ">
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger brh-header-font-color" href="#about">Sobre</a>
+            </li>                   
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger brh-header-font-color" href="#portfolio">Portfolio</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger brh-header-font-color" href="#services">Serviços</a>
-            </li>
+            </li>     
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger brh-header-font-color" href="#portfolio">Portfolio</a>
+              <a class="nav-link js-scroll-trigger brh-header-font-color" href="#register">Cadastro</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger brh-header-font-color" href="#contact">Contato</a>
@@ -69,7 +74,7 @@
             <hr>
           </div>
           <div class="col-lg-8 mx-auto">
-            <p class="text-faded mb-5 brh-header-font-color">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+            <p class="text-faded mb-5 brh-header-font-color">Quer Saber Como?</p>
             <a class="btn brh-button-color btn-xl js-scroll-trigger" href="#about">Leia Mais</a>
           </div>
         </div>
@@ -92,74 +97,6 @@
         </div>
       </div>
     </section>
-
-    <section id="info">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading"></h2>
-            <form>
-              <div class="form-group">                
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" 
-                placeholder="Nome">                
-              </div>
-              <div class="form-group">                
-                <input type="email" class="form-control" id="exampleInputPassword1" 
-                placeholder="Email">
-                <small id="emailHelp" class="form-text text-muted">Fique tranquilo(a), nós nunca enviamos span's para sua caixa postal.</small>
-              </div>
-              <div class="form-check">                
-              </div>
-              <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
-            <hr class="my-4">
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- <section id="services">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading">At Your Service</h2>
-            <hr class="my-4">
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-1"></i>
-              <h3 class="mb-3">Sturdy Templates</h3>
-              <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-paper-plane text-primary mb-3 sr-icon-2"></i>
-              <h3 class="mb-3">Ready to Ship</h3>
-              <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-code text-primary mb-3 sr-icon-3"></i>
-              <h3 class="mb-3">Up to Date</h3>
-              <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-4x fa-heart text-primary mb-3 sr-icon-4"></i>
-              <h3 class="mb-3">Made with Love</h3>
-              <p class="text-muted mb-0">You have to make your websites with love these days!</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
 
     <section class="p-0" id="portfolio">
       <div class="container-fluid p-0">
@@ -256,17 +193,84 @@
           </div>
         </div>
       </div>
+    </section>     
+
+    <section id="services">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Nossos Serviços</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row brh-logo">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-gem  mb-3 sr-icon-1"></i>
+              <h3 class="mb-3">Recrutamento e Seleção sempre Igualitárias</h3>
+              <p class="text-muted mb-0">Processos seletivos em parceria com as melhores empresas, todas com visão de inclusão profissional.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-paper-plane mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">Parceria na capacitação Profisional</h3>
+              <p class="text-muted mb-0">Promoções e descontos em cursos e palestras para acelerar a sua entrada no mercado de trabalho</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-code mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">Aproximação das empresas e talentos</h3>
+              <p class="text-muted mb-0">Links com as melhoras vagas, para você talento, e para as empresas, sempre os melhores colaboradores.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-heart mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Criamos oportunidades de igualdade social</h3>
+              <p class="text-muted mb-0">E sim, tudo feito com amor ao proximo, gerando oportunidades, chances e fazendo a diferança sempre s2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="register">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading"></h2>
+            <form>
+              <div class="form-group">                
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" 
+                placeholder="Nome">                
+              </div>
+              <div class="form-group">                
+                <input type="email" class="form-control" id="exampleInputPassword1" 
+                placeholder="Email">
+                <small id="emailHelp" class="form-text text-muted">Fique tranquilo(a), nós nunca enviamos span's para sua caixa postal.</small>
+              </div>
+              <div class="form-check">                
+              </div>
+              <button type="submit" class="btn btn-primary brh-button-color">Enviar</button>
+            </form>
+            <hr class="brh-detail">
+          </div>
+        </div>
+      </div>
     </section>
 
     <section class="bg-dark text-white">
-      <div class="container text-center">
-        <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
-        <a class="btn btn-light btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Download Now!</a>        
-        <a class="btn btn-social-icon btn-twitter">
-          <span class="fa fa-twitter"></span>
-        </a>
-      </div>
-    </section>
+        <div class="container text-center">
+          <h2 class="mb-4">O futuro pertence àqueles que se preparam hoje para ele.</h2>
+          <h3 class="mb-4">(Malcolm X)</h3>
+          <a class="btn btn-light btn-xl sr-button" href="#">Nosso Blog</a>
+          </a>
+        </div>
+      </section> 
 
     <section id="contact">
       <div class="container">
@@ -274,7 +278,7 @@
           <div class="col-lg-8 mx-auto text-center">
             <h2 class="section-heading">Estamos te esperando</h2>
             <h3 class="section-heading">Ubuntu!</h3>
-            <hr class="my-4">
+            <hr class="my-4" id="brh-detail-hr">
             <p class="mb-5">Quer saber mais sobre essa ferramenta de revolução no mercado de trabalho?</p>
             <p class="mb-5">
               Entre em contato por email, cadastro ou por whatsapp, e consiga as melhores oportunidades 
@@ -285,18 +289,17 @@
         <div class="row">
           <div class="col-lg-4 ml-auto text-center">
             <i class="fas fa-phone fa-3x mb-3 sr-contact-1"></i>
-            <p>(55 - 41)99876 - 2848</p>
+            <p id="brh-text">(55 - 41)99876 - 2848</p>
           </div>
           <div class="col-lg-4 mr-auto text-center">
             <i class="fas fa-envelope fa-3x mb-3 sr-contact-2"></i>
             <p> 
-              <a href="mailto:contato@blackrh.com">contato@blackrh.com</a>
+              <a href="mailto:contato@blackrh.com" id="brh-font-color">contato@blackrh.com</a>
             </p>
           </div>
         </div>
       </div>
     </section>
-
     <!-- Bootstrap core JavaScript -->
     <script src="style/theme/vendor/jquery/jquery.min.js"></script>
     <script src="style/theme/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -308,7 +311,31 @@
 
     <!-- Custom scripts for this template -->
     <script src="style/theme/js/creative.min.js"></script>
-
   </body>
+  <script>
+  $("#registerWorker").click(function() {
+  	  var Name = 	$('#name').val();
+      var Email = 	$('#email').val();      
+      if(userId == "" || userId == null){
+          alert("Erro no Id");
+      }else{            
+    	  $.ajax({
+			  method: "POST",
+			  url: "../index.php",
+			  data: {
+		            method: 'registerWorker',
+		            parameter : [userId, total ]
+		          },
+		  }).done(function( msg ) {			  
+			  if(msg){
+				  alert("ok");
+			  }
+			  else{
+				  alert("erro");
+			}
+		  });
+ 		 }
+  });
+  </script>
 
 </html>
